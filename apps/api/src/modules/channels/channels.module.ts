bd3dca68d3312cbb5a12ca118be0c46269f343f7
obamenzogho <nzoghoobameb@gmail.com>
2026-08-17 21:59:54 -0400

    fix(api): importer StorageModule dans ChannelsModule (ChannelsService injecte StorageService)

--- apps/api/src/modules/channels/channels.module.ts
+++ apps/api/src/modules/channels/channels.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { StorageModule } from '../../common/storage/storage.module';
 import { StreamingModule } from '../streaming/streaming.module';
 import { ChannelsController } from './channels.controller';
 import { ChannelsService } from './channels.service';
 
 @Module({
-  imports: [StreamingModule],
+  imports: [StreamingModule, StorageModule],
   controllers: [ChannelsController],
   providers: [ChannelsService],
 })
