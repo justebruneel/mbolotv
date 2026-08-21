875479efcb8def64bdb471e6fac4436afacf8f88
justebruneel <justebruneel@gmail.com>
2026-08-21 19:25:40 -0400

    feat: require active access for channels

--- apps/api/src/modules/channels/channels.controller.ts
+++ apps/api/src/modules/channels/channels.controller.ts
@@ -1,44 +1,23 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import type {
-  Channel,
-  ChannelListResponse,
-  ChannelQuery,
-  CountryOption,
-  PlayResponse,
-  Programme,
-} from '@mbolo/contracts';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import type { Channel, ChannelListResponse, ChannelQuery, CountryOption, PlayResponse, Programme } from '@mbolo/contracts';
 import { channelQuerySchema } from '@mbolo/contracts';
+import { AccessGuard } from '../access/access.guard';
 import { ZodValidationPipe } from '../../common/pipes/zod-validation.pipe';
 import { ChannelsService } from './channels.service';
 
+@UseGuards(AccessGuard)
 @Controller('channels')
 export class ChannelsController {
   constructor(private readonly channelsService: ChannelsService) {}
 
   @Get()
-  list(
-    @Query(new ZodValidationPipe(channelQuerySchema)) query: ChannelQuery,
-  ): Promise<ChannelListResponse> {
-    return this.channelsService.list(query);
-  }
-
+  list(@Query(new ZodValidationPipe(channelQuerySchema)) query: ChannelQuery): Promise<ChannelListResponse> { return this.channelsService.list(query); }
   @Get('countries')
-  countries(): Promise<CountryOption[]> {
-    return this.channelsService.countries();
-  }
-
+  countries(): Promise<CountryOption[]> { return this.channelsService.countries(); }
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<Channel> {
-    return this.channelsService.findOne(id);
-  }
-
+  findOne(@Param('id') id: string): Promise<Channel> { return this.channelsService.findOne(id); }
   @Get(':id/epg')
-  epg(@Param('id') id: string): Promise<Programme[]> {
-    return this.channelsService.epg(id);
-  }
-
+  epg(@Param('id') id: string): Promise<Programme[]> { return this.channelsService.epg(id); }
   @Get(':id/play')
-  play(@Param('id') id: string): Promise<PlayResponse> {
-    return this.channelsService.play(id);
-  }
+  play(@Param('id') id: string): Promise<PlayResponse> { return this.channelsService.play(id); }
 }
