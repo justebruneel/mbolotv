17f72a24360d84abdec83a9bebf79a0b3fe48044
justebruneel <justebruneel@gmail.com>
2026-08-21 19:25:18 -0400

    feat: wire access guard for categories

--- apps/api/src/modules/categories/categories.module.ts
+++ apps/api/src/modules/categories/categories.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AccessModule } from '../access/access.module';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 
 @Module({
+  imports: [AccessModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
