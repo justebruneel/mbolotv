7666b2bb73e93454ed5c8caac1d8eb8491e1ae05
justebruneel <justebruneel@gmail.com>
2026-08-21 19:25:46 -0400

    feat: wire access guard for channels

--- apps/api/src/modules/channels/channels.module.ts
+++ apps/api/src/modules/channels/channels.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StorageModule } from '../../common/storage/storage.module';
+import { AccessModule } from '../access/access.module';
 import { StreamingModule } from '../streaming/streaming.module';
 import { ChannelsController } from './channels.controller';
 import { ChannelsService } from './channels.service';
 
 @Module({
-  imports: [StreamingModule, StorageModule],
+  imports: [AccessModule, StreamingModule, StorageModule],
   controllers: [ChannelsController],
   providers: [ChannelsService],
 })
